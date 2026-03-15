04592dc23dd2698965930dd85671e35cf77dd15e
Alireza Maleklou <maleklou.a@mvmco.ir>
2026-03-15 21:44:03 +0330

    Create User Register,Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,6 +4,9 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using TaskFlow.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Api.Models;
 
 namespace TaskFlow.Api.Controllers
 {
@@ -13,20 +16,24 @@ namespace TaskFlow.Api.Controllers
     {
 
         private readonly IConfiguration _config;
-        public AuthController(IConfiguration config) 
+        private readonly AppDbContext _appDbContext;
+
+        public AuthController(IConfiguration config, AppDbContext appDbContext) 
         {
           _config = config;
+          _appDbContext = appDbContext;
         }
 
         [HttpPost("login")]
-        public IActionResult Login([FromBody] LoginDto dto)
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
         {
-            if(dto.Username == "admin" && dto.Password == "12345")
-            {
-                var token = GenerateJwtToken(dto.Username);
-                return Ok(new { Token = token });
-            }
-            return Unauthorized();
+            var user =await _appDbContext.Users.FirstOrDefaultAsync(u => u.Username == dto.Username);
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash))
+                return Unauthorized();
+
+            var token = GenerateJwtToken(user.Username);
+            return Ok(new { Token = token });
         }
 
         private string GenerateJwtToken(string username)
@@ -34,7 +41,7 @@ namespace TaskFlow.Api.Controllers
             var claims = new[]
             {
             new Claim(ClaimTypes.Name, username)
-        };
+            };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -48,6 +55,29 @@ namespace TaskFlow.Api.Controllers
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
+        {
+            var existUser = await _appDbContext.Users.AnyAsync(u => u.Username == dto.Username);
+            if(existUser)
+            {
+                return BadRequest("This Username Already Existed !!!");
+            }
+
+            var user = new User
+            {
+                Username = dto.Username,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+
+            };
+
+           await _appDbContext.Users.AddAsync(user);
+           await _appDbContext.SaveChangesAsync();
+
+            return Ok("User Created Successfully !");
         }
     }
 }
